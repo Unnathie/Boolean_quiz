d81454641dd600636d0a1f0423c4025c13f8bb72
Unnathi E <123557096+Unnathie@users.noreply.github.com>
2025-08-08 18:24:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 from question_model import Questions
 from quiz_brain import QuizBrain
-from data import question_data
+from data import question_data,funfact_data
 questionbank=[]
-for ques in question_data:
-    questionbank.append(Questions(ques["question"],ques["correct_answer"]))
+choices=int(input("Choose your quiz mode:\nPress 1 for Fun Facts.\nPress 2 for Trivia Challenge."))
+if choices==1:
+    for ques in funfact_data:
+        questionbank.append(Questions(ques["text"],ques["answer"]))
+
+else:
+    for ques in question_data:
+        questionbank.append(Questions(ques["question"], ques["correct_answer"]))
+
+
 
 quiz=QuizBrain(questionbank)
-# for questions in questionbank:
 while quiz.still_has_questions():
     quiz.next_question()
 print(f"You completed the quiz! \nYOUR SCORE:{quiz.score}/{len(questionbank)}")
